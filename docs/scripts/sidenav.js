2c4da60ae4aee77534065ccfb667bb7bb690cd01
Glaeder27 <90222344+Glaeder27@users.noreply.github.com>
2025-08-02 00:00:10 +0200

    Fixes

--- docs/scripts/sidenav.js
+++ docs/scripts/sidenav.js
@@ -33,14 +33,14 @@ function initializeMenu() {
   } else {
     toggleButton.style.transform = "translate(0, 0)";
   }
-  toggleIcon.className = menuVisible ? "fas fa-chevron-left" : "fas fa-chevron-right";
+  toggleIcon.className = menuVisible ? "fas fa-square-xmar" : "fas fa-right-from-bracket";
 
   toggleButton.addEventListener("click", () => {
     const wasCollapsed = sideMenu.classList.contains("collapsed");
     const isNowVisible = sideMenu.classList.toggle("collapsed") === false;
     localStorage.setItem("menuVisible", isNowVisible);
 
-    toggleIcon.className = isNowVisible ? "fas fa-chevron-left" : "fas fa-chevron-right";
+    toggleIcon.className = isNowVisible ? "fas fa-square-xmark" : "fas fa-right-from-bracket";
 
     // Reset animation
     toggleButton.classList.remove("animate-open", "animate-close");
