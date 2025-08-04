c5a2c96fd2decf248e43832f333c9c809e752373
Glaeder27 <90222344+Glaeder27@users.noreply.github.com>
2025-08-04 11:03:17 +0200

    User page

---
+++ docs/scripts/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADcrS6zihFsQNcCCc5qdjJdyK3GuXcy_c",
+  authDomain: "seeker-ttrpg.firebaseapp.com",
+  projectId: "seeker-ttrpg",
+  storageBucket: "seeker-ttrpg.firebasestorage.app",
+  messagingSenderId: "762210490643",
+  appId: "1:762210490643:web:a2b3893b9e4cfbdc8862e6",
+  measurementId: "G-F3HS8JCNLT"
+};
