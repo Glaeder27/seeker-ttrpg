05ba6922dcb8b2160c03d6e35d1bc12d65b04d51
Glaeder27 <contact@seekerttrpg.com>
2025-08-09 23:07:58 +0200

    Update 1036 • 09/08/25
    Files changed:
    docs/assets/images/banners/meeting-of-cortes-and-moctezuma.webp
    docs/index.html
    docs/scripts/parallax.js

--- docs/scripts/parallax.js
+++ docs/scripts/parallax.js
@@ -1,4 +1,4 @@
-/*v1.3 2025-08-09T20:39:21.681Z*/
+/*v1.4 2025-08-09T20:47:04.802Z*/
 document.addEventListener("DOMContentLoaded", () => {
   const parallaxElements = document.querySelectorAll(".parallax-vertical, .parallax-horizontal");
 
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const crossSpeed = speed * 0.1; // 10% dello speed
       
       // Calcolo zoom: aumenta con la distanza dal centro
-      const zoomFactor = 1 + Math.min(Math.abs(offsetFromCenter) / viewportHeight, 0.5) * 0.2;
-      // ↑ max 20% di zoom quando l’elemento è molto lontano dal centro
+      const zoomFactor = 1 + Math.min(Math.abs(offsetFromCenter) / viewportHeight, 0.5) * 0.1;
+      // ↑ max 10% di zoom quando l’elemento è molto lontano dal centro
 
       if (el.classList.contains("parallax-vertical")) {
         el.style.backgroundPosition =
