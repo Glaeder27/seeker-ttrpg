d5a0855bf82ba52471f1488c9bd271aa8c348061
Glaeder27 <contact@seekerttrpg.com>
2025-08-09 21:14:00 +0200

    Update 1034 • 09/08/25
    Files changed:
    docs/scripts/parallax.js

--- docs/scripts/parallax.js
+++ docs/scripts/parallax.js
@@ -1,4 +1,4 @@
-/*v1.1 2025-08-09T15:56:56.764Z*/
+/*v1.2 2025-08-09T19:13:56.935Z*/
 document.addEventListener("DOMContentLoaded", () => {
   const parallaxElements = document.querySelectorAll(".parallax-vertical, .parallax-horizontal");
 
@@ -11,11 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
       const offsetFromCenter = elementCenterY - viewportHeight / 2;
 
       const speed = parseFloat(el.dataset.speed) || 0.3;
+      const crossSpeed = speed * 0.1; // 10% dello speed
 
       if (el.classList.contains("parallax-vertical")) {
-        el.style.backgroundPosition = `center calc(50% + ${-offsetFromCenter * speed}px)`;
-      } else if (el.classList.contains("parallax-horizontal")) {
-        el.style.backgroundPosition = `calc(50% + ${-offsetFromCenter * speed}px) center`;
+        el.style.backgroundPosition = 
+          `calc(50% + ${-offsetFromCenter * crossSpeed}px) calc(50% + ${-offsetFromCenter * speed}px)`;
+      } 
+      else if (el.classList.contains("parallax-horizontal")) {
+        el.style.backgroundPosition = 
+          `calc(50% + ${-offsetFromCenter * speed}px) calc(50% + ${-offsetFromCenter * crossSpeed}px)`;
       }
     });
   });
